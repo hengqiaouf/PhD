c1e2c4689ec2c6a0a2f00113b9ad5fd6797bf7ab
hengqiaouf <ayqiaoheng@gmail.com>
2019-11-19 22:49:50 -0500

    autoencoder

--- Firma/GAN_LSTM/lstm_autoencoder/autoencoder.py
+++ Firma/GAN_LSTM/lstm_autoencoder/autoencoder.py
@@ -3,25 +3,33 @@ import torch.nn as nn
 import torch.optim as optim
 #import dataread
 import os
-from dataread import FirmaData
+from dataread import FirmaData_onesubject
 from torch.utils.data import Dataset, DataLoader
 
 # hyper-parameters
 b_size = 32
 window_size = 10  # size of sliding window, or sequence length
-subject_id = 1
+subject_id_train = 1
+subject_id_anomaly=3
 latent_dim = 20  # dimension of hidden state in GRU
 num_layer = 1  # number of layers of GRU
 learning_rate = 0.005
 Max_epoch = 3
 # data set up
 cur_dir = os.getcwd()
-data_folder_dir = os.path.join(cur_dir, "datamat")
-dataset = FirmaData(data_folder_dir, subject_id, window_size)
-train_loader = DataLoader(dataset, batch_size=b_size, shuffle=True)
+data_folder_dir = os.path.join(cur_dir, "../../data")
+cuda=torch.device('cuda:0')
+
+dataset_train = FirmaData_onesubject(data_folder_dir, subject_id_train, 0.7, 0.0, 0.3, window_size, subset='train')
+dataset_test_normal=FirmaData_onesubject(data_folder_dir,subject_id_train,0.7,0.0,0.3,window_size,subset='test')
+dataset_test_anomaly = FirmaData_onesubject(data_folder_dir, subject_id_anomaly, 0.7, 0.0, 0.3, window_size, subset='train')
+
+loader_train = DataLoader(dataset_train, batch_size=b_size, shuffle=True)
+loader_test_normal=DataLoader(dataset_test_normal, batch_size=b_size, shuffle=True)
+loader_test_anomaly=DataLoader(dataset_test_anomaly,batch_size=b_size,shuffle=True)
 #
 # [window_size,data_dim]  564 in our case (remove the first timestamp dimension)
-input_size = dataset[0].shape[1]
+input_size = dataset_train[0].shape[1]
 
 class Encoder(nn.Module):
     def __init__(self, input_dim, latent_dim, num_layers):
@@ -29,34 +37,72 @@ class Encoder(nn.Module):
         self.input_dim = input_dim
         self.latent_dim = latent_dim
         self.num_layers = num_layers
-
         self.encoder = nn.GRU(
             self.input_dim, self.latent_dim, self.num_layers, batch_first=True)
     def forward(self,encode_input):
-         _, last_hidden = self.encoder(encode_input)
+        _, last_hidden = self.encoder(encode_input)
         return last_hidden
 class Decoder(nn.Module):
-     def __init__(self, input_dim, latent_dim,output_dim, num_layers): #output_dim should be the same with input_dim
+     def __init__(self, input_dim, latent_dim, output_dim, num_layers): #output_dim should be the same with input_dim
         super(Decoder, self).__init__()
         self.input_dim = input_dim
         self.latent_dim = latent_dim
         self.num_layers = num_layers
         self.output_dim = output_dim
-        self.decoder = nn.GRU(
-            self.input_dim, self.latent_dim, self.num_layers, batch_first=True)
-        self.decode_out = nn.Linear(latent_dim, output_dim) 
+        self.decoder = nn.GRU(self.input_dim, self.latent_dim, self.num_layers, batch_first=True)
+        self.decode_out = nn.Linear(latent_dim, output_dim)
     def forward(self,decode_input,incode_hidden):
-        decode_input=decode_input.unsqueeze(1)# from [batch_size,input_size] to [batch_sizem,1,input_size]
-        out,_=self.decoder(decode_input,incode_hidden) # when using nn.LSTM, the decode_input should be [batch_size,seq_len,data_dim] but here we decode one step at a time, so seq_len should always be 1. 
+        decode_input=decode_input.unsqueeze(1)# from [batch_size,input_size] to [batch_size,1,input_size]
+        out,last_hidden=self.decoder(decode_input,incode_hidden) # when using nn.LSTM, the decode_input should be [batch_size,seq_len,data_dim] but here we decode one step at a time, so seq_len should always be 1.
 #out should have shape [batch_size,seq_len,latent_dim*n directions] but here seq_len=1
         seq_gen=self.decode_out(out.squeeze(1)) # we use batch_first so the seq_len dimension is dimension 1, so we use out.squeeze(1) to make the output [batch_size,latent_dim], and the seq_gen [batch_size,output_dim]
-        return seq_gen #[batch_size, output_dim]
+        return seq_gen,last_hidden # seq_gen:[batch_size, output_dim]
 
-model = FirmaGRU(input_dim=input_size, latent_dim=latent_dim, num_layers=num_layer)
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        assert encoder.latent_dim == decoder.latent_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.num_layers == decoder.num_layers, \
+            "Encoder and decoder must have equal number of layers!"
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        # src = [ batch size,src sent len,data_dim]
+        # trg = [ batch size,trg sent len,data_dim]
+        # teacher_forcing_ratio is probability to use teacher forcing
+        # e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
+        batch_size = trg.shape[0]
+        max_len = trg.shape[1]
+        data_size = self.decoder.output_dim
+        # tensor to store decoder outputs
+        outputs = torch.zeros(max_len,batch_size, data_size).to(self.device) # we need to reshape this into [batch_size,max_len,data_size] later
+        # last hidden state of the encoder is used as the initial hidden state of the decoder
+        hidden= self.encoder(src)
+        # first input to the decoder zero, having same shape as the first step of input
+        input = torch.zeros([batch_size,data_size])
+        for t in range(1, max_len):
+            # insert input token embedding, previous hidden and previous cell states
+            # receive output tensor (predictions) and new hidden and cell states
+            output, hidden = self.decoder(input, hidden)
+            # place predictions in a tensor holding predictions for each token
+            outputs[t] = output
+            # decide if we are going to use teacher forcing or not
+            teacher_force = random.random() < teacher_forcing_ratio
+            # if teacher forcing, use actual current source sequence step
+            # if not, use predicted token
+            next_true_in=trg[:,t,:]
+            input = next_true_in if teacher_force else output #narrow: slice from the 1 st dimension(seq_len), starting from t th step, with length 1
+        return outputs
+model_encoder=Encoder(input_size,latent_dim,num_layer)
+model_decoder=Decoder(input_size,latent_dim,,input_size,num_layer)
+model = Seq2Seq(model_encoder,model_decoder,cuda)
 loss_function = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 for epoch in range(Max_epoch):
-    for step, seq_data in enumerate(train_loader):
+    for step, seq_data in enumerate(loader_train):
 #        print(seq_data.shape)  # [32,10,564], [batch,seq_len,data_dim]
         seq_pred = model(seq_data.float())
         loss = loss_function(seq_pred, seq_data)
--- Firma/GAN_LSTM/lstm_autoencoder/dataread.py
+++ Firma/GAN_LSTM/lstm_autoencoder/dataread.py
@@ -4,32 +4,134 @@ import os
 import glob
 
 
-class FirmaData(Dataset):
-    def __init__(self, data_folder_dir, sub_id, window_size,pre_process=True):
+class FirmaData_onesubject(Dataset):
+    def __init__(self, data_folder_dir, sub_id, train_part, val_part, test_part, window_size, subset='train',
+                 pre_process=True):
         matfiles = []
+        assert train_part + val_part + test_part == 1  # partition of three sub datasets
         for f in os.listdir(data_folder_dir):
             if f.endswith(".npy"):
                 matfiles.append(f)
         matfiles.sort()
         file_dir = os.path.join(data_folder_dir, matfiles[sub_id])
         self.datamat = np.load(file_dir)
-        self.window_size=window_size
-        if(pre_process):
+        self.window_size = window_size
+        if (pre_process):
             # remove first dimension
             self.datamat = self.datamat[:, 1:]
             # binarilize
             self.datamat = np.float64(self.datamat > 0)
+        setsize = self.datamat.shape[0] - self.window_size
+        train_idx = int(setsize * train_part)
+        val_idx = int(setsize * (train_part + val_part))
+        if (subset == 'train'):
+            self.datamat = self.datamat[:train_idx]
+        if (subset == 'val'):
+            self.datamat = self.datamat[train_idx:val_idx]
+        if (subset == 'test'):
+            self.datamat = self.datamat[val_idx:]
 
     def __len__(self):
-        return self.datamat.shape[0]-(self.window_size)
+        return self.datamat.shape[0] - (self.window_size)
 
     def __getitem__(self, idx):
-        
-        return self.datamat[idx:idx+(self.window_size), :]
+        return self.datamat[idx:idx + (self.window_size), :]
+
+
+class FirmaData_select_subjects(Dataset):
+    def __init__(self, data_folder_dir, window_size, train_part, val_part, test_part, subject_list, subset='train',
+                 pre_process=True):  # subset = train, test, val
+        matfiles = []
+        assert train_part + val_part + test_part == 1  # partition of three sub datasets
+        self.window_size = window_size
+        for f in os.listdir(data_folder_dir):
+            if f.endswith(".npy"):
+                matfiles.append(f)
+        matfiles.sort()
+        self.subset = subset
+        self.data = []
+        self.label = []
+        for sub_id in subject_list:
+            file_dir = os.path.join(data_folder_dir, matfiles[sub_id])
+            self.datamat = np.load(file_dir)
+            if pre_process == True:
+                # remove first dimension
+                self.datamat = self.datamat[:, 1:]
+                # binarilize
+                self.datamat = np.float64(self.datamat > 0)
+            data_temp = []
+            label_temp = []
+            for idx in range(self.datamat.shape[0] - (self.window_size)):
+                data_temp.append(self.datamat[idx:idx + (self.window_size), :].squeeze(0))
+                label_temp.append(sub_id)
+            matsize = len(data_temp)
+            train_idx = int(train_part * matsize)
+            val_idx = int((train_part + val_part) * matsize)
+            if self.subset == 'train':
+                self.data = self.data + data_temp[:train_idx]
+                self.label = self.label + label_temp[:train_idx]
+            if self.subset == 'val':
+                self.data = self.data + data_temp[train_idx:val_idx]
+                self.label = self.label + label_temp[train_idx:val_idx]
+            if self.subset == 'test':
+                self.data = self.data + data_temp[val_idx:]
+                self.label = self.label + label_temp[val_idx:]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):  # sample size: seq_len,data_dim: [60,564]
+        return self.data[idx], self.label[idx]
+
+
+class FirmaData_all_subjects(Dataset):
+    def __init__(self, data_folder_dir, window_size, train_part, val_part, test_part, subset='train',
+                 pre_process=True):  # subset = train, test, val
+        matfiles = []
+        assert train_part + val_part + test_part == 1  # partition of three sub datasets
+        self.window_size = window_size
+        for f in os.listdir(data_folder_dir):
+            if f.endswith(".npy"):
+                matfiles.append(f)
+        matfiles.sort()
+        self.subset = subset
+        self.data = []
+        self.label = []
+        for sub_id in range(17):
+            file_dir = os.path.join(data_folder_dir, matfiles[sub_id])
+            self.datamat = np.load(file_dir)
+            if pre_process == True:
+                # remove first dimension
+                self.datamat = self.datamat[:, 1:]
+                # binarilize
+                self.datamat = np.float64(self.datamat > 0)
+            data_temp = []
+            label_temp = []
+            for idx in range(self.datamat.shape[0] - (self.window_size)):
+                data_temp.append(self.datamat[idx:idx + (self.window_size), :])
+                label_temp.append(sub_id)
+            matsize = len(data_temp)
+            train_idx = int(train_part * matsize)
+            val_idx = int((train_part + val_part) * matsize)
+            if self.subset == 'train':
+                self.data = self.data + data_temp[:train_idx]
+                self.label = self.label + label_temp[:train_idx]
+            if self.subset == 'val':
+                self.data = self.data + data_temp[train_idx:val_idx]
+                self.label = self.label + label_temp[train_idx:val_idx]
+            if self.subset == 'test':
+                self.data = self.data + data_temp[val_idx:]
+                self.label = self.label + label_temp[val_idx:]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):  # sample size: seq_len,data_dim: [60,564]
+        return self.data[idx], self.label[idx]
 
 
 if __name__ == '__main__':
     cur_dir = os.getcwd()
-    data_folder_dir = os.path.join(cur_dir, "datamat")
-    dataset = FirmaData(data_folder_dir, 1,2)
-    print(dataset[3])
+    data_folder_dir = os.path.join(cur_dir, "../../data")
+    dataset = FirmaData_onesubject(data_folder_dir, 1, 0.7, 0.0, 0.3, 60, subset='train')
+    print(dataset.__len__())
