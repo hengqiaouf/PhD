2495bfb4912a2072e99e9fecac883fa33840b27f
hengqiaouf <ayqiaoheng@gmail.com>
2019-11-21 10:39:41 -0500

    lstm

--- Firma/Multi_Class/LSTM/train.py
+++ Firma/Multi_Class/LSTM/train.py
@@ -1,54 +1,55 @@
 import os
-import sys
 import argparse
-import time
 import random
-import pdb
-
 import torch
-import torch.autograd as autograd
 import torch.nn as nn
-import torch.functional as F
 import torch.optim as optim
-
 from dataread import *
-
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from model import LSTMClassifier
+import matplotlib.pyplot as plt
+import numpy as np
 
+#fix random seeds
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(0)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--save_path',type=str,default='./saved_model/shared_data/1_week')
-    parser.add_argument('--data_dir', type=str, default='../../data',
+    parser.add_argument('--running_mode', type=str, default='test', help='training or testing the model')
+    parser.add_argument('--load_id', type=int, default=0, help='which model to load for test')
+    parser.add_argument('--save_path', type=str, default='./saved_model')
+    parser.add_argument('--data_dir', type=str, default='../../data/shared_data/1_week',
                         help='data_directory')
-    parser.add_argument('--subset_par',type=list,default=[0.5,0.2,0.3],
+    parser.add_argument('--subset_par', type=list, default=[0.6, 0.1, 0.3],
                         help='partition of training, validation and test sets')
     parser.add_argument('--hidden_dim', type=int, default=100,
                         help='LSTM hidden dimensions')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='size for each minibatch')
-    parser.add_argument('--num_epochs', type=int, default=20,
+    parser.add_argument('--num_epochs', type=int, default=10,
                         help='maximum number of epochs')
     parser.add_argument('--input_dim', type=int, default=564,
                         help='input dimension')
-    parser.add_argument('--learning_rate', type=float, default=0.0001,
+    parser.add_argument('--learning_rate', type=float, default=0.0005,
                         help='initial learning rate')
     parser.add_argument('--weight_decay', type=float, default=1e-4,
                         help='weight_decay rate')
     parser.add_argument('--seed', type=int, default=123,
                         help='seed for random initialisation')
     args = parser.parse_args()
-    train(args)
+    if args.running_mode == 'train':
+        train(args)
+    elif args.running_mode == 'test':
+        test(args)
+    else:
+        print('provide correct running mode!')
 
 
-def apply(model, criterion, batch, targets, lengths):
-    pred = model(torch.autograd.Variable(batch), lengths.cpu().numpy())
-    loss = criterion(pred, torch.autograd.Variable(targets))
-    return pred, loss
-
-def train_model(model, optimizer, train, val, max_epochs,savepath):
+def train_model(model, optimizer, train, val, max_epochs, savepath):
     criterion = nn.NLLLoss()
     for epoch in range(max_epochs):
         model.train()
@@ -56,94 +57,109 @@ def train_model(model, optimizer, train, val, max_epochs,savepath):
         y_true = list()
         y_pred = list()
         total_loss = 0
-        for step, (seq_data,label) in enumerate (train):
+        for step, (seq_data, label) in enumerate(train):
             model.zero_grad()
-            seq_data=seq_data.cuda()
-            label= label.cuda()
-            label_pre=model(seq_data.float())
-#            label_pre=model(seq_data.cuda())
+            seq_data = seq_data.cuda()
+            label = label.cuda()
+            label_pre = model(seq_data.float())
+            #            label_pre=model(seq_data.cuda())
 
-            loss_step=criterion(label_pre,label)
+            loss_step = criterion(label_pre, label)
             loss_step.backward()
             optimizer.step()
             pred_idx = torch.max(label_pre, 1)[1]
             y_true += list(label.cpu())
-            
+
             y_pred += list(pred_idx.cpu().data.int())
             total_loss += loss_step
-            if(step % 200 ==0) :
-                print('train step:',step)
-                print('accuracy of current batch: ',accuracy_score(label.cpu(),pred_idx.cpu().data.int()))
+            # if(step % 200 ==0) :
+            #     print('train step:',step)
+            #     print('accuracy of current batch: ',accuracy_score(label.cpu(),pred_idx.cpu().data.int()))
         acc = accuracy_score(y_true, y_pred)
         val_loss, val_acc = evaluate_val_set(
             model, val, criterion)
-        print("Epoch: {}   Train loss: {} - acc: {} \nValidation loss: {} - acc: {}".format(epoch,total_loss.data.float()/len(train), acc,val_loss, val_acc))
+        print("Epoch: {}   Train loss: {} - acc: {} \nValidation loss: {} - acc: {}".format(epoch,
+                                                                                            total_loss.data.float() / len(
+                                                                                                train), acc, val_loss,
+                                                                                            val_acc))
         torch.save({
-                'epoch':epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': total_loss,
-        },os.path.join(savepath,'model_'+str(epoch)+'.tar'))
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': total_loss,
+        }, os.path.join(savepath, 'model_' + str(epoch) + '.tar'))
     return model
 
-def evaluate_val_set(model, dat_loader_val,criterion):
+
+def evaluate_val_set(model, dat_loader_val, criterion):
     model.eval()
     y_true = list()
     y_pred = list()
     total_loss = 0
-    for step, (seq_data,label) in enumerate(dat_loader_val):
-        if(step % 200 ==0):
-            print('val step:',step)
-#        print(seq_data.shape) [batch,seq_size,data_dim]
-        seq_data=seq_data.cuda()
-        label=label.cuda()
-        label_pre = model(seq_data.float())
-        loss_step=criterion(label_pre,label)
-        total_loss+=loss_step
-        y_true+=list(label.cpu())
-        pred_idx=torch.max(label_pre, 1)[1]
-        y_pred+=list(pred_idx.cpu().data.int())
-    acc=accuracy_score(y_true,y_pred)
-    return total_loss.data.float()/step, acc
+    with torch.no_grad():
+        for step, (seq_data, label) in enumerate(dat_loader_val):
+            # if(step % 200 ==0):
+            #     print('val step:',step)
+            seq_data = seq_data.cuda()
+            label = label.cuda()
+            label_pre = model(seq_data.float())
+            loss_step = criterion(label_pre, label)
+            total_loss += loss_step
+            y_true += list(label.cpu())
+            pred_idx = torch.max(label_pre, 1)[1]
+            y_pred += list(pred_idx.cpu().data.int())
+        acc = accuracy_score(y_true, y_pred)
+    return total_loss.data.float() / step, acc
 
 
-def evaluate_test_set(model, test, x_to_ix, y_to_ix):
+def evaluate_test_set(model, dat_loader_test):
+    model.eval()
     y_true = list()
     y_pred = list()
-
-    for batch, targets, lengths, raw_data in utils.create_dataset(test, x_to_ix, y_to_ix, batch_size=1):
-        batch, targets, lengths = utils.sort_batch(batch, targets, lengths)
-
-        pred = model(torch.autograd.Variable(batch), lengths.cpu().numpy())
-        pred_idx = torch.max(pred, 1)[1]
-        y_true += list(targets.int())
-        y_pred += list(pred_idx.data.int())
-
-    print(len(y_true), len(y_pred))
-    print(classification_report(y_true, y_pred))
-    print(confusion_matrix(y_true, y_pred))
+    with torch.no_grad():
+        for step, (seq_data, label) in enumerate(dat_loader_test):
+            seq_data = seq_data.cuda()
+            label = label.cuda()
+            label_pre = model(seq_data.float())
+            y_true += list(label.cpu())
+            pred_idx = torch.max(label_pre, 1)[1]
+            y_pred += list(pred_idx.cpu().data.int())
+    acc = accuracy_score(y_true, y_pred)
+    plot_conf_mat=False
+    conf_mat = confusion_matrix(y_true, y_pred)
+    if plot_conf_mat:
+        fig,ax=plt.subplots()
+        im = ax.imshow(conf_mat, interpolation='nearest', cmap=plt.cm.Blues)
+        ax.figure.colorbar(im, ax=ax)
+        plt.show()
+    return acc, conf_mat
 
 
 def train(args):
     random.seed(args.seed)
-    dataset_train=FirmaData_all_subjects(args.data_dir,60,args.subset_par[0],args.subset_par[1],args.subset_par[2],subset='train')
-    dataset_val=FirmaData_all_subjects(args.data_dir,60,args.subset_par[0],args.subset_par[1],args.subset_par[2],subset='val')
-    dataset_test=FirmaData_all_subjects(args.data_dir,60,args.subset_par[0],args.subset_par[1],args.subset_par[2],subset='test')
-    dat_loader_train=DataLoader(dataset_train,batch_size=args.batch_size,shuffle=True)
-    dat_loader_val=DataLoader(dataset_val,batch_size=args.batch_size,shuffle=True)
-    dat_loader_test=DataLoader(dataset_test,batch_size=args.batch_size,shuffle=True)
-
-
-
-    model = LSTMClassifier (args.input_dim,
-                           args.hidden_dim, output_size=17)
-  #  optimizer = optim.SGD(        model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    dataset_train = FirmaData_all_subjects(args.data_dir, 60, args.subset_par[0], args.subset_par[1],
+                                           args.subset_par[2], subset='train', pre_process=False)
+    dataset_val = FirmaData_all_subjects(args.data_dir, 60, args.subset_par[0], args.subset_par[1], args.subset_par[2],
+                                         subset='val', pre_process=False)
+    dat_loader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True)
+    dat_loader_val = DataLoader(dataset_val, batch_size=args.batch_size, shuffle=True)
+    model = LSTMClassifier(args.input_dim, args.hidden_dim, output_size=17)
     model.cuda()
-    optimizer = optim.Adam(model.parameters(),lr=args.learning_rate)
-    model = train_model(model, optimizer, dat_loader_train, dat_loader_val,args.num_epochs,args.save_path)
-
-#    evaluate_test_set(model, test_data, char_vocab, tag_vocab)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    train_model(model, optimizer, dat_loader_train, dat_loader_val, args.num_epochs, args.save_path)
 
 
+def test(args):
+    dataset_test = FirmaData_all_subjects(args.data_dir, 60, args.subset_par[0], args.subset_par[1], args.subset_par[2],
+                                          subset='test', pre_process=False)
+    dat_loader_test = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=True)
+    for loadid in range(args.num_epochs):
+        saved_model = os.path.join(args.save_path, 'model_' + str(loadid) + '.tar')
+        checkpoint = torch.load(saved_model)
+        model = LSTMClassifier(args.input_dim, args.hidden_dim, output_size=17)
+        model.cuda()
+        model.load_state_dict(checkpoint['model_state_dict'])
+        acc,_= evaluate_test_set(model, dat_loader_test)
+        print('model {} test_accuracy:{:5.4f}'.format(loadid,acc))
 if __name__ == '__main__':
     main()
